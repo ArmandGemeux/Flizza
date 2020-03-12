6e1420577231c8d572a7c983dbfdce570146ff80
ArmandGemeux <armandgemeux@hotmail.fr>
2020-03-12 10:56:14 +0100

    SplashScreen & Changement graphique

---
+++ Assets/MyScripts/Swipe.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Swipe : MonoBehaviour
+{
+    private float swipeDistanceThreshold = 50;
+
+    private Vector2 startPosition;
+    private Vector2 endPosition;
+    private void Update()
+    {
+        if (Input.touchCount == 1)
+        {
+            var touch = Input.touches[0];
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    // Stockage du point de départ
+                    startPosition = touch.position;
+                    break;
+                case TouchPhase.Ended:
+                    // Stockage du point de fin
+                    endPosition = touch.position;
+                    AnalyzeGesture(startPosition, endPosition);
+                    break;
+            }
+        }
+    }
+
+    private void AnalyzeGesture(Vector2 start, Vector2 end)
+    {
+        // Distance
+        if (Vector2.Distance(start, end) > swipeDistanceThreshold)
+        {
+            // Le mouvement est suffisamment ample
+            SceneManager.LoadScene("01_SceneJeu");
+        }
+    }
+
+}
