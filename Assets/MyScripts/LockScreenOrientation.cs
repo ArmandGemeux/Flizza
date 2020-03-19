2bd0337579a0d32bec80f934687157bb6666c4c9
ArmandGemeux <armandgemeux@hotmail.fr>
2020-03-19 20:39:41 +0100

    Ajout scène de commande, polish UI again, celui qui lit est une belle personne

---
+++ Assets/MyScripts/LockScreenOrientation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LockScreenOrientation : MonoBehaviour
+{
+    public void Awake()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
